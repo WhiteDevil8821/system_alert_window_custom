b97b1ba29fff14fc2d9e9eb130c8af5593b717cc
WhiteDevil8821 <ankit.kumar221099@gmail.com>
2025-04-19 21:09:07 +0530

    Updated SystemAlertWindowPlugin and SystemAlertWindowService

--- android/src/main/java/in/jvapps/system_alert_window/SystemAlertWindowPlugin.java
+++ android/src/main/java/in/jvapps/system_alert_window/SystemAlertWindowPlugin.java
@@ -1,181 +1,74 @@
 package in.jvapps.system_alert_window;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import in.jvapps.system_alert_window.utils.Constants;
-import in.jvapps.system_alert_window.utils.ContextHolder;
-import in.jvapps.system_alert_window.utils.LogUtils;
-import io.flutter.FlutterInjector;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.embedding.engine.FlutterEngineCache;
-import io.flutter.embedding.engine.FlutterEngineGroup;
-import io.flutter.embedding.engine.dart.DartExecutor;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.BasicMessageChannel;
-import io.flutter.plugin.common.JSONMessageCodec;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
-public class SystemAlertWindowPlugin implements FlutterPlugin, ActivityAware, BasicMessageChannel.MessageHandler {
+/** SystemAlertWindowPlugin */
+public class SystemAlertWindowPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
-    private boolean isInitialized;
-
-    @Nullable
-    private ActivityPluginBinding pluginBinding;
-    MethodCallHandlerImpl methodCallHandler;
-    private final String TAG = "SAW:Plugin";
-    private Context context;
-    private BasicMessageChannel<Object> messenger;
-
-    public SystemAlertWindowPlugin() {
-        LogUtils.getInstance().d(TAG, "Initializing the constructor");
-        isInitialized = false;
-    }
-
-    private void initialize(FlutterPluginBinding binding) {
-        ContextHolder.setApplicationContext(binding.getApplicationContext());
-        if (!isInitialized) {
-            isInitialized = true;
-            LogUtils.getInstance().d(TAG, "Initializing on attached to engine");
-            if (methodCallHandler == null) {
-                methodCallHandler = new MethodCallHandlerImpl();
-                methodCallHandler.startListening(binding.getBinaryMessenger());
-            }
-            LogUtils.getInstance().d(TAG, "onAttachedToEngine");
-        }
-    }
-
-    private void registerListeners() {
-        if (pluginBinding != null) {
-            pluginBinding.addActivityResultListener(methodCallHandler);
-        }
-    }
-
-    private void deregisterListeners() {
-        if (pluginBinding != null) {
-            pluginBinding.removeActivityResultListener(methodCallHandler);
-        }
-    }
-
-    private void dispose() {
-        LogUtils.getInstance().d(TAG, "Disposing call track plugin class");
-        if (methodCallHandler != null) {
-            methodCallHandler.stopListening();
-            methodCallHandler.setActivity(null);
-            methodCallHandler = null;
-        }
-        isInitialized = false;
-    }
-
-    private void startForegroundNotification() {
-        String CHANNEL_ID = "system_alert_channel";
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "System Alert Window Service",
-                    NotificationManager.IMPORTANCE_LOW);
-            // NotificationManager manager = getSystemService(NotificationManager.class);
-            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            if (manager != null) {
-                manager.createNotificationChannel(channel);
-            }
-        }
-
-        // Notification notification = new Notification.Builder(this, CHANNEL_ID)
-        Notification notification = new Notification.Builder(context, CHANNEL_ID)
-                .setContentTitle("Overlay Running")
-                .setContentText("The overlay window is active")
-                .setSmallIcon(context.getApplicationInfo().icon) // You can replace with a custom small icon
-                .build();
-
-        startForeground(1001, notification);
-    }
-
-    
+    private MethodChannel channel;
+    private Context applicationContext;
+    private Activity activity;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        this.context = flutterPluginBinding.getApplicationContext();
-        initialize(flutterPluginBinding);
-        messenger = new BasicMessageChannel<>(flutterPluginBinding.getBinaryMessenger(), Constants.MESSAGE_CHANNEL,
-                JSONMessageCodec.INSTANCE);
-        messenger.setMessageHandler(this);
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "system_alert_window");
+        channel.setMethodCallHandler(this);
+        applicationContext = flutterPluginBinding.getApplicationContext();
     }
 
     @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        if (!isInitialized) {
-            LogUtils.getInstance().d(TAG, "Already detached from the engine.");
-            return;
-        }
-        LogUtils.getInstance().d(TAG, "On detached from engine");
-        dispose();
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
     }
 
     @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-        LogUtils.getInstance().d(TAG, "Initializing on attached to activity");
-
-        // Existing code...
-        if (methodCallHandler != null) {
-            methodCallHandler.setActivity(activityPluginBinding.getActivity());
-
-            try {
-                FlutterEngine existingEngine = FlutterEngineCache.getInstance().get(Constants.FLUTTER_CACHE_ENGINE);
-                if (existingEngine == null) {
-                    FlutterEngineGroup enn = new FlutterEngineGroup(context);
-                    DartExecutor.DartEntrypoint dEntry = new DartExecutor.DartEntrypoint(
-                            FlutterInjector.instance().flutterLoader().findAppBundlePath(),
-                            "overlayMain");
-                    FlutterEngine engine = enn.createAndRunEngine(context, dEntry);
-                    FlutterEngineCache.getInstance().put(Constants.FLUTTER_CACHE_ENGINE, engine);
-                }
-            } catch (Exception e) {
-                LogUtils.getInstance().e(TAG, "Error initializing FlutterEngine: " + e.getMessage());
-            }
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("startOverlayService")) {
+            startOverlayService();
+            result.success("Foreground service started");
+        } else {
+            result.notImplemented();
         }
+    }
 
-        this.pluginBinding = activityPluginBinding;
-        registerListeners();
+    private void startOverlayService() {
+        Intent serviceIntent = new Intent(applicationContext, SystemAlertWindowService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            applicationContext.startForegroundService(serviceIntent);
+        } else {
+            applicationContext.startService(serviceIntent);
+        }
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
     }
 
     @Override
     public void onDetachedFromActivityForConfigChanges() {
-        onDetachedFromActivity();
+        activity = null;
     }
 
     @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
-        onAttachedToActivity(activityPluginBinding);
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
     }
 
     @Override
     public void onDetachedFromActivity() {
-        LogUtils.getInstance().d(TAG, "On detached from activity");
-        if (methodCallHandler != null) {
-            methodCallHandler.setActivity(null);
-        }
-        deregisterListeners();
+        activity = null;
     }
-
-    @Override
-    public void onMessage(@Nullable Object message, @NonNull BasicMessageChannel.Reply reply) {
-        BasicMessageChannel overlayMessageChannel = new BasicMessageChannel(
-                FlutterEngineCache.getInstance().get(Constants.FLUTTER_CACHE_ENGINE)
-                        .getDartExecutor(),
-                Constants.MESSAGE_CHANNEL, JSONMessageCodec.INSTANCE);
-        overlayMessageChannel.send(message, reply);
-    }
-
-    
-
 }
